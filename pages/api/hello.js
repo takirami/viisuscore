83c84a918358f7684cfe433128dc43163f7b75e3
engstroj <jonas.engstrom@nordsoftware.com>
2021-05-20 20:39:15 +0300

    Api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,43 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const contentful = require("contentful-management");
+const client = contentful.createClient({
+  accessToken: "CFPAT-ozi2uOE9kBZV0abdPFOLAdqiFmZiQaLHSRjRwvhj7sw"
+});
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const spaceId = '2qgpnp14mgvm'
+
+export const Create = () => {
+
+client.getSpace(spaceId)
+.then((space) => space.getEnvironment('master'))
+.then((environment) => environment.createEntryWithId('score', 'bar', {
+  fields: {
+      jonas: {
+        'en-US': 69     
+      }  
+  }
+}))
+.then((entry) => console.log(entry))
+.catch(console.error)
+}
+
+export const Update = () => {
+// Update entry
+client.getSpace(spaceId)
+.then((space) => space.getEnvironment('master'))
+.then((environment) => environment.getEntry('foo'))
+.then((entry) => {
+  entry.fields.jonas['en-US'] = 420
+  return entry.update()
+})
+.then((entry) => console.log(`Entry ${entry.sys.id} updated.`))
+.catch(console.error)
+
+}
+export const getAllEntries = () => {
+
+client.getSpace(spaceId)
+.then((space) => space.getEnvironment('master'))
+.then((environment) => environment.getEntries())
+.then((response) => console.log(response.items))
+.catch(console.error)  
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
+import axios from 'axios'
+import { isUpdated } from 'contentful-management'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Update, Create, getAllEntries} from '../pages/api/hello'
+
+const requesttest = async() => {
+ const result = await Update()
+ console.log(result)
+}
+
+const getVotes = async () => {
+  const result = await getAllEntries()
+  console.log(result)
+}
 
 export default function Home() {
   return (
@@ -15,41 +28,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+<button onClick={() => requesttest()}>TEST</button>
+<button onClick={() => getVotes()}>getAllEntries</button>
       </main>
 
       <footer className={styles.footer}>
